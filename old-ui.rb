26cb09f04392c98e4ed6c9b283310ebeaa4a8607
issam khairullah <issamok3@gmail.com>
2021-04-02 18:25:53 +0300

    Changed syntax from if-elsif to case statement.

--- old-ui.rb
+++ old-ui.rb
@@ -1,12 +1,23 @@
 puts 'What do you want to do? [read | write | exit]'
 action = gets.chomp
 
-if action == 'read'
-  puts "Entering #{action} mode ....."
-elsif action == 'write'
-  puts "Entering #{action} mode ....."
-elsif action == 'exit'
-  puts "Entering #{action} mode ....."
+# if action == 'read'
+#   puts "Entering #{action} mode ....."
+# elsif action == 'write'
+#   puts "Entering #{action} mode ....."
+# elsif action == 'exit'
+#   puts "Entering #{action} mode ....."
+# else
+#   puts "Incorrect choice!"
+# end
+
+case action
+when "read"
+  puts "Entering read mode...."
+when "write"
+  puts "Entering write mode...."
+when "exit"
+  puts "Goodbye"
 else
-  puts "Incorrect choice!"
+  puts "Incorrect choice"
 end
